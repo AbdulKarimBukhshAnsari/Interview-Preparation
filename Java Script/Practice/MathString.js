c93056923a14a550402b0bedac8e50ea40464338
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2025-07-23 21:20:10 +0500

    Basic JS is Completed (Math , String , Few Concepts of Coercion)

---
+++ Java Script/Practice/MathString.js
@@ -0,0 +1,64 @@
+
+// 🔢 Math Object Practice Questions
+// Generate a random integer between 1 and 50.
+
+// Round 4.6 to the nearest integer.
+
+// Find the absolute value of -25.
+
+// Find the smallest number in this list: 5, -3, 9, 0, -10.
+
+// Calculate the square root of 144.
+
+// Calculate 3 raised to the power of 4.
+
+// Simulate rolling a dice (number between 1 and 6).
+
+// Round 3.2 up to the nearest integer.
+
+// Round 7.8 down to the nearest integer.
+
+// Generate a random integer between 100 and 200.
+
+// 🔤 String Methods Practice Questions
+// Find the length of the string "JavaScript Interview".
+
+// Change "hello world" to all uppercase letters.
+
+// Extract the substring "Script" from "JavaScript".
+
+// Replace the word "cat" with "dog" in "The cat sat on the mat".
+
+// Check if the string "frontend" contains the substring "end".
+
+// Remove whitespace from both ends of the string " clean me ".
+
+// Split the string "apple,banana,orange" into an array of fruits.
+
+// Get the first character of the string "React".
+
+// Combine the strings "Hello" and "World" into one string.
+
+// Convert the string "JavaScript" to all lowercase letters.
+
+
+// random Integer 
+console.log(Math.floor(Math.random() * 50) + 1) ;
+
+//Rounding Function 
+console.log(Math.round(4.6)) ;
+
+//Min Number 
+console.log(Math.min(1,2,3,0,-1 , -2)) ;
+
+// Substring Contain 
+
+console.log("frontend".includes('end'));
+
+// trim end 
+console.log("  end  ".trim());
+
+console.log("apple,banana,orange".split(","));
+
+
+
