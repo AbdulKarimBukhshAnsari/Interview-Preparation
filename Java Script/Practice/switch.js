c883a48cbb7e5c7ce38bae3d29efb3116c1dad5b
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2025-07-24 02:59:57 +0500

    Implemented Custom UseState and few other difference

---
+++ Java Script/Practice/switch.js
@@ -0,0 +1,12 @@
+let day = "Wednesday" ;
+
+switch (day) {
+    case "Monday" :
+        console.log("Start of the Week") ;
+        break ;
+    case "Friday" :
+        console.log("End of the week");
+        break ;
+    default :
+        console.log("Mid week");
+} ;
