704872c96577acbf0cba900763b32a75af6b5c46
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2025-09-14 23:55:43 +0500

    removed the practice sheet in JS

--- Java Script/Practice/SliceAndSplice.js
+++
@@ -1,9 +0,0 @@
-let arr = ["a" , "b" , "c" , "d"] ;
-let arr1 = arr.slice(0,2) ;
-console.log("Modified Array after Slice",arr1) // ["a" , "b"] -> Expected
-console.log("Old Array" , arr) ; // ["a" , "b" , "c" , "d"] -> Expected 
-let arr2 = arr.splice(1,2 , "e" , "f" , "h") ;  
-console.log("Modified Array by using Splice" , arr2) ; // ["b" , "c" ] -> Expected 
-console.log("Old Array" , arr); // ["a" , "d"] -> Expected 
-
-// Old Expected output was right 
